ea6416bd590e3eba9ea863aab9eaa51b375b75ab
Nick Lyman <nick.lyman@gmail.com>
2016-03-25 16:53:18 -0700

    Finalized styles, updated html links,and updated scoring values.

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,25 @@
 $(document).ready(function() {
   $("form#vacation-goals").submit(function(event) {
     event.preventDefault();
-    var answer1 = parseInt($("input:radio[name=q1]:checked").val());
-    var answer2 = parseInt($("input:radio[name=q2]:checked").val());
-    var answer3 = parseInt($("input:radio[name=q3]:checked").val());
-    var answer4 = parseInt($("input:radio[name=q4]:checked").val());
-    var answer5 = parseInt($("input:radio[name=q5]:checked").val());
+    var answer1 = parseInt($("input:radio[name=question1]:checked").val());
+    var answer2 = parseInt($("input:radio[name=question2]:checked").val());
+    var answer3 = parseInt($("input:radio[name=question3]:checked").val());
+    var answer4 = parseInt($("input:radio[name=question4]:checked").val());
+    var answer5 = parseInt($("input:radio[name=question5]:checked").val());
     var total = 0;
     total = (answer1+answer2+answer3+answer4+answer5)
     if (0 <= total && total <=5) {
-      $(location).attr("href", "option1.html")
+      $(location).attr("href", "el-chalten.html")
     } else if (6 <= total && total <=25) {
-      $(location).attr("href", "option2.html")
+      $(location).attr("href", "vancouver-bc.html")
     } else if (total >= 26) {
-      $(location).attr("href", "option3.html")
+      $(location).attr("href", "dubai.html")
     }
   });
 });
+
+
+//I want to incorporate the profile information below but am having trouble incorporating two form types in this scripts.js file
 
 // $(document).ready(function() {
 //   $("form#profile").submit(function(event) {
