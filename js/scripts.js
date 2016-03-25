3c90118368ed662fc1f414fa0f96f7f76abac367
Nick Lyman <nick.lyman@gmail.com>
2016-03-25 12:12:14 -0700

    Update styles, add JavaScript, and vacation destination images.

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+  $("form#vacation-goals").submit(function() {
+    event.preventDefault();
+    var answer1 = parseInt($("input:radio[name=q1]:checked").val());
+    var answer2 = parseInt($("input:radio[name=q2]:checked").val());
+    var answer3 = parseInt($("input:radio[name=q3]:checked").val());
+    var answer4 = parseInt($("input:radio[name=q4]:checked").val());
+    var answer5 = parseInt($("input:radio[name=q5]:checked").val());
+    var total = 0;
+    total = (answer1+answer2+answer3+answer4+answer5)
+    if (0 <= total && total <=5) {
+      $(location).attr("href", "img/el-chalten.jpg").css({"position":"relative"})
+    } else if (6 <= total && total <=25) {
+      $(location).attr("href", "img/vancouver-bc.jpg")
+    } else if (total > 25) {
+      $(location).attr("href", "img/abu-dhabi.jpg")
+    }
+  });
+});
